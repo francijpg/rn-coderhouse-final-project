fc39b3b1d37c7293484fd3e1333b972b78b1edf6
francijpg <hellofrancijpg@gmail.com>
2022-07-17 16:11:42 -0400

    chore: install and config initial dependencies

---
+++ app.config.js
@@ -0,0 +1,44 @@
+import 'dotenv/config';
+
+export default {
+    "expo": {
+    "name": "rn-coderhouse-final-project",
+    "slug": "rn-coderhouse-final-project",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./src/assets/icon.png",
+    "userInterfaceStyle": "light",
+    "splash": {
+      "image": "./src/assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+    "updates": {
+      "fallbackToCacheTimeout": 0
+    },
+    "assetBundlePatterns": [
+      "**/*"
+    ],
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./src/assets/adaptive-icon.png",
+        "backgroundColor": "#FFFFFF"
+      }
+    },
+    "web": {
+      "favicon": "./src/assets/favicon.png"
+    },
+    extra: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APPID
+    } 
+  }
+}
---
+++ src/config/firebase.js
@@ -0,0 +1,28 @@
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { getApp, initializeApp } from "firebase/app";
+
+import Constants from "expo-constants";
+
+const firebaseConfig = {
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  databaseURL: Constants.manifest.extra.databaseURL,
+  proyectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId,
+};
+
+function createFirebaseApp(config) {
+  try {
+    return getApp();
+  } catch {
+    return initializeApp(config);
+  }
+}
+
+const firebaseApp = createFirebaseApp(firebaseConfig);
+
+export const auth = getAuth(firebaseApp);
+export const signIn = signInWithEmailAndPassword;
+export const createUser = createUserWithEmailAndPassword;
