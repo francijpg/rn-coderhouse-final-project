22a6f14af5a091112c5524692bb482b0c3e8bfaf
francijpg <hellofrancijpg@gmail.com>
2022-07-17 16:12:16 -0400

    chore: create navigation

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AppNavigation from "./src/navigation/AppNavigation";
+
+export default function App() {
+  return (
+    <AppNavigation />
+  );
+}
---
+++ src/navigation/AppNavigation.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/LoginScreen";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
