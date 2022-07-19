42ab5e3299023f309e41182b2b88de4ddbc4fcb5
francijpg <hellofrancijpg@gmail.com>
2022-07-19 14:32:21 -0400

    Create colors.js

---
+++ src/constants/colors.js
@@ -0,0 +1,12 @@
+export const colors = {
+  primary: "#310A31",
+  secondary: "#847996",
+  text: "#212121",
+  background: "#ffffff",
+  shadow: "#000000",
+  white: "#ffffff",
+  whiteOpacity: "#ffffff90",
+  black: "#000000",
+  gray: "#f3f3f3",
+  error: "#ff0000",
+};
