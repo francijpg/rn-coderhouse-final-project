4287b71adda185a00ab9b407eea991e5efe896af
francijpg <hellofrancijpg@gmail.com>
2022-07-20 12:16:53 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { Provider } from "react-redux";
 import AppNavigation from "./src/navigation/AppNavigation";
+import store from "./src/store";
 
 export default function App() {
   return (
-    <AppNavigation />
+    <Provider store={store}>
+      <AppNavigation />
+    </Provider>
   );
 }
