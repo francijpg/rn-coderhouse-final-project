4f6f1e84400cd9e2c2a0e6ea65ed666cec621699
francijpg <hellofrancijpg@gmail.com>
2022-07-21 19:44:28 -0400

    chore: create custom label

---
+++ src/components/label/Label.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./label.styles";
+
+const Label = ({ children, style, label, subLabel, subLabelStyle }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.label, style]}>{label}</Text>
+      {children}
+      {subLabel && (
+        <Text style={[styles.subLabel, subLabelStyle]}>{subLabel}</Text>// TODO remove unnecessary styles
+      )}
+    </View>
+  );
+};
+
+export default Label;
---
+++ src/components/label/label.styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../constants/colors";
+
+export const styles = StyleSheet.create({
+  // container: {
+  //   marginTop: 20,
+  // },
+  label: {
+    fontSize: 17,
+    color: colors.white,
+    // fontFamily: "Lato-Bold",
+    fontWeight: "400", // TODO replace for fontFamily: "Lato-Bold",
+  },
+});
