96fad2423ad795b12668bdc3ecb45c0e657a3958
francijpg <hellofrancijpg@gmail.com>
2022-07-24 00:26:19 -0400

    Update auth.actions.js

--- src/store/actions/auth.actions.js
+++ src/store/actions/auth.actions.js
@@ -1,20 +1,25 @@
 import { authTypes } from "../types/auth.types";
+const { SIGN_UP, SIGN_IN, SIGN_OUT, RESTORE_TOKEN } = authTypes;
 
-const { SIGN_UP, SIGN_IN } = authTypes;
-
-import { auth, fbSignUp, fbSignIn } from "../../services/firebase";
+import { auth, fbSignUp, fbSignIn, fbSignOut } from "../../services/firebase";
+import { setToken, deleteToken } from "../../utils/token";
 
 export const signUp = (email, password) => {
   return async (dispatch) => {
     try {
-      const data = await fbSignUp(auth, email, password);
+      const { _tokenResponse: response } = await fbSignUp(
+        auth,
+        email,
+        password
+      );
+      await setToken(response.idToken);
       dispatch({
         type: SIGN_UP,
-        token: data.idToken,
-        userId: data.localId,
+        userToken: response.idToken,
+        userId: response.localId,
       });
     } catch (error) {
-      console.log(error);
+      console.log(`signUp error: ${error}`);
     }
   };
 };
@@ -22,14 +27,46 @@ export const signUp = (email, password) => {
 export const signIn = (email, password) => {
   return async (dispatch) => {
     try {
-      const data = await fbSignIn(auth, email, password);
+      const { _tokenResponse: response } = await fbSignIn(
+        auth,
+        email,
+        password
+      );
+      await setToken(response.idToken);
       dispatch({
         type: SIGN_IN,
-        token: data.idToken,
-        userId: data.localId,
+        userToken: response.idToken,
+        userId: response.localId,
       });
     } catch (error) {
-      console.log(error);
+      console.log(`signIn error: ${error}`);
+    }
+  };
+};
+
+export const signOut = () => {
+  return async (dispatch) => {
+    try {
+      await fbSignOut(auth);
+      await deleteToken();
+      dispatch({
+        type: SIGN_OUT,
+      });
+    } catch (error) {
+      console.log(`signOut error: ${error}`);
+    }
+  };
+};
+
+export const restoreSession = (token) => {
+  return async (dispatch) => {
+    try {
+      dispatch({
+        type: RESTORE_TOKEN,
+        userToken: token,
+      });
+    } catch (error) {
+      console.log(`restoreSession error: ${error}`);
     }
   };
 };
