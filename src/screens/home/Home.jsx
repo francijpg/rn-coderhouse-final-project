508c04e03364a83ec29427d5be52bfad108c89a7
francijpg <hellofrancijpg@gmail.com>
2022-07-25 11:57:20 -0400

    fix: update Home Screen

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -3,21 +3,32 @@ import { Text, View, TouchableOpacity } from "react-native";
 import { useSelector } from "react-redux";
 import { signOut } from "../../store/actions/auth.actions";
 import { useDispatch } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
   const auth = useSelector((state) => state.auth);
-  console.log(auth.userId);
+  const navigation = useNavigation();
   const dispatch = useDispatch();
-
+  // console.log(auth);
   const handleSignOut = () => {
     dispatch(signOut());
   };
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <Text>Home Screen</Text>
+      <Text>{auth.userId}</Text>
       <TouchableOpacity onPress={handleSignOut}>
         <Text>Sign Out</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
+        <Text>Camera</Text>
       </TouchableOpacity>
     </View>
   );
