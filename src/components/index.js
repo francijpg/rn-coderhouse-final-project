f382c2880f92fbc826b5867e81e02a640ad171f9
francijpg <hellofrancijpg@gmail.com>
2022-07-26 10:31:16 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Label } from "./label/Label";
 export { default as Input } from "./input/Input";
 export { default as BgImage } from "./background/BgImage";
 export { default as Shapes } from "./background/Shapes";
+export { default as ButtonIcon } from "./buttonIcon/ButtonIcon";
