651cf46b8a4f3e2a68574ed27c1ed28f130541e8
francijpg <hellofrancijpg@gmail.com>
2022-07-27 00:02:46 -0400

    Create messages.js

---
+++ src/constants/messages.js
@@ -0,0 +1,7 @@
+export const messages = {
+  signInError: "Credenciales inválidas",
+  signUpError: "Error al registrarse",
+  signOutError: "Error al cerrar sesión",
+  restoreSessionError: "Error al restaurar sesión",
+  restoreSessionSuccess: "Sesión restaurada",
+};
