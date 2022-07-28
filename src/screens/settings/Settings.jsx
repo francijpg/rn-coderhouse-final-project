8742d0cfc0121534dc4763fe266c3e18846a6cdc
francijpg <hellofrancijpg@gmail.com>
2022-07-28 00:01:19 -0400

    Create Settings.jsx

---
+++ src/screens/settings/Settings.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { styles } from "./settings.style";
+
+const SettingsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Settings Screen</Text>
+    </View>
+  );
+};
+
+export default SettingsScreen;
