c8feb81122513bd2457ca430c524428b6d3ca932
francijpg <hellofrancijpg@gmail.com>
2022-07-28 00:01:10 -0400

    Update ButtonIcon.jsx

--- src/components/buttonIcon/ButtonIcon.jsx
+++ src/components/buttonIcon/ButtonIcon.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, TouchableOpacity } from "react-native";
-import { Entypo } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 import { styles } from "./buttonIcon.style";
 import { colors } from "../../constants/colors";
 
@@ -8,7 +8,7 @@ export default function ButtonIcon({ title, onPress, icon, color }) {
   const buttonTextColor = color ? color : colors.white;
   return (
     <TouchableOpacity onPress={onPress} style={styles.button}>
-      <Entypo name={icon} size={28} color={buttonTextColor} />
+      <FontAwesome name={icon} size={28} color={buttonTextColor} />
       <Text style={[styles.text, { color: buttonTextColor }]}>{title}</Text>
     </TouchableOpacity>
   );
