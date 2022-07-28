965bd8951f5b544841e694f325aa585a399686c6
francijpg <hellofrancijpg@gmail.com>
2022-07-28 00:01:26 -0400

    Update Splash.jsx

--- src/screens/splash/Splash.jsx
+++ src/screens/splash/Splash.jsx
@@ -4,7 +4,7 @@ import { styles } from "./splash.style";
 export default function Splash() {
   return (
     <View style={styles.screenContainer}>
-      <Text style={styles.title}>Bienvenida</Text>
+      <Text style={styles.title}>ChatApp</Text>
       <ActivityIndicator size="large" />
     </View>
   );
