bdc26a7be4d703041c6ff1e65d101c8dec72aba2
francijpg <hellofrancijpg@gmail.com>
2022-07-29 00:07:48 -0400

    Update BgImage.jsx

--- src/components/background/BgImage.jsx
+++ src/components/background/BgImage.jsx
@@ -4,9 +4,11 @@ import { Image, StyleSheet } from "react-native";
 import { styles } from "./background.style";
 
 const BgImage = () => {
-  const uri = "https://ak.picdn.net/shutterstock/videos/1060308725/thumb/1.jpg";
   return (
-    <Image source={{ uri }} style={[styles.image, StyleSheet.absoluteFill]} />
+    <Image
+      source={require("../../assets/images/background.jpg")}
+      style={[styles.image, StyleSheet.absoluteFill]}
+    />
   );
 };
 
