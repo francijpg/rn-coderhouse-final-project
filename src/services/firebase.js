4ded0108e2c0eefc7193d6f8257371dd1af9a1b3
francijpg <hellofrancijpg@gmail.com>
2022-07-30 00:15:01 -0400

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,6 +5,7 @@ import {
   signOut,
 } from "firebase/auth";
 import { getApp, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 import Constants from "expo-constants";
 
@@ -12,7 +13,7 @@ const firebaseConfig = {
   apiKey: Constants.manifest.extra.apiKey,
   authDomain: Constants.manifest.extra.authDomain,
   databaseURL: Constants.manifest.extra.databaseURL,
-  proyectId: Constants.manifest.extra.projectId,
+  projectId: Constants.manifest.extra.projectId,
   storageBucket: Constants.manifest.extra.storageBucket,
   messagingSenderId: Constants.manifest.extra.messagingSenderId,
   appId: Constants.manifest.extra.appId,
@@ -32,3 +33,4 @@ export const auth = getAuth(firebaseApp);
 export const fbSignIn = signInWithEmailAndPassword;
 export const fbSignUp = createUserWithEmailAndPassword;
 export const fbSignOut = signOut;
+export const fbDatabase = getFirestore();
