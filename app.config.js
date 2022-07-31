e53422b557f432b57a9a7f058a9110c7f8111f33
francijpg <hellofrancijpg@gmail.com>
2022-07-31 12:37:07 -0400

    add assets

--- app.config.js
+++ app.config.js
@@ -1,35 +1,41 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
-    "expo": {
-    "name": "rn-coderhouse-final-project",
-    "slug": "rn-coderhouse-final-project",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./src/assets/app/icon.png",
-    "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./src/assets/app/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+  expo: {
+    name: "chat-app",
+    slug: "chat-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./src/assets/app/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./src/assets/app/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    "updates": {
-      "fallbackToCacheTimeout": 0
+    updates: {
+      fallbackToCacheTimeout: 0,
     },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./src/assets/app/adaptive-icon.png",
-        "backgroundColor": "#FFFFFF"
-      }
+    android: {
+      package: "com.francijpg.coderhouse",
+      versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: "./src/assets/app/adaptive-icon.png",
+        backgroundColor: "#FFFFFF",
+      },
+      permissions: [
+        "CAMERA",
+        "CAMERA_ROLL",
+        "LOCATION",
+        "WRITE_EXTERNAL_STORAGE",
+      ],
     },
-    "web": {
-      "favicon": "./src/assets/app/favicon.png"
+    web: {
+      favicon: "./src/assets/app/favicon.png",
     },
     extra: {
       apiKey: process.env.FIREBASE_API_KEY,
@@ -38,7 +44,7 @@ export default {
       projectId: process.env.FIREBASE_PROJECT_ID,
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APPID
-    } 
-  }
-}
+      appId: process.env.FIREBASE_APPID,
+    },
+  },
+};
